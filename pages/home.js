0e0f386f14b0f83eae9ce44b42f6acab0cc26ac9
unknown <omis8f@gmail.com>
2021-10-21 11:20:35 +0200

    header content

---
+++ pages/home.js
@@ -0,0 +1,13 @@
+//react komponente se pišu velikom početnim slovom
+//function Home(){
+//    return <h1>Hello home</h1>;
+//}
+
+import Header from "../modules/header/header";
+
+//const Home = () => <h1>Hello home</h1>
+
+// ako tag nema djece može se koristit ovakav zapis <Header />
+const Home = () => <Header />;
+
+export default Home;
